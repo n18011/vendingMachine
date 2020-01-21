7dc5596ada4d2853a56c1746157f75d8b15bb712
n18011 <n18011@std.it-college.ac.jp>
2020-01-21 21:18:36 +0900

    add class

---
+++ vendingMachine.py
@@ -0,0 +1,16 @@
+class VendingMachine:
+    def __init__(self):
+
+    # 飲み物をセットする
+    def set_drink(self):
+    # 飲み物を表示する
+    def drink_display(self):
+    # 投入金額を表示する
+    def money_display(self):
+    # 飲み物を出す
+    def put_out_drink(self):
+    # お釣りを出す
+    def change(self):
+
+
+if __name__ == '__main__':
